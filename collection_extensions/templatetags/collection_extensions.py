b81ea7047dc47598a915f4941800a673083a1e33
722C <722C@users.noreply.github.com>
2018-12-07 17:54:32 -0700

    Add some additional template tags.

--- collection_extensions/templatetags/collection_extensions.py
+++ collection_extensions/templatetags/collection_extensions.py
@@ -1,5 +1,8 @@
 from django import template
 
+from ..models import CollectionExtension
+
+
 register = template.Library()
 
 
@@ -7,3 +10,21 @@ register = template.Library()
                         takes_context=True)
 def collection_extensions_side_nav(context):
     return context
+
+
+@register.filter
+def collection_alternative_name(collection):
+    if collection:
+        ce = CollectionExtension.objects.filter(collection=collection).first()
+        if ce:
+            return ce.alternative_name
+    return ''
+
+
+@register.filter
+def collection_content(collection):
+    if collection:
+        ce = CollectionExtension.objects.filter(collection=collection).first()
+        if ce:
+            return ce.content
+    return ''
